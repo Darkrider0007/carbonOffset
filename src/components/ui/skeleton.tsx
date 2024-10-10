4325b590ad15c850e16fbce201191b5189fa85af
Rohan Gope <rg2512@it.jgec.ac.in>
2024-10-10 21:38:49 +0530

    fixing bugs

--- src/components/ui/skeleton.tsx
+++ src/components/ui/skeleton.tsx
@@ -1,9 +1,12 @@
-import { cn } from "@/lib/utils"
-
+import
+{
+  cn
+} from "../../lib/utils"
 function Skeleton({
   className,
   ...props
-}: React.HTMLAttributes<HTMLDivElement>) {
+}: React.HTMLAttributes<HTMLDivElement>)
+{
   return (
     <div
       className={cn("animate-pulse rounded-md bg-muted", className)}
